29f63d222c7809c6dfd9e4c8f422971454d99c66
steve williams <sjwilliams51@gmail.com>
2024-08-08 10:59:58 -0400

    Initial Commit

---
+++ CFLTax/CFLTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFLTaxApp.swift
+//  CFLTax
+//
+//  Created by Steven Williams on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CFLTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CFLTax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CFLTax
+//
+//  Created by Steven Williams on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
